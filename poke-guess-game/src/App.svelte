0a7159ca1dc3ef70fda11da740ca45db0e7fdf68
joshdotcc <josh@josh.cc>
2024-12-18 11:25:00 -0700

    Created Main Components

--- poke-guess-game/src/App.svelte
+++ poke-guess-game/src/App.svelte
@@ -1,30 +1,8 @@
 <script>
-	export let name;
+  import Autocomplete from './Autocomplete.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Pokémon Guessing Game</h1>
+  <Autocomplete />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
