e873a84b5b9521fddd54960547dcaca04006ccf5
joshdotcc <josh@josh.cc>
2024-12-18 21:56:31 -0700

    Tigheting up and reformatting my messy CSS

--- poke-guess-game/src/App.svelte
+++ poke-guess-game/src/App.svelte
@@ -1,21 +1,7 @@
 <script>
   import Autocomplete from './Autocomplete.svelte';
-  import GuessFeedback from './GuessFeedback.svelte';
-
-  let userGuess = ''; // Variable to store the user's guess
-
-  // Handle the custom event 'guessSelected' from Autocomplete
-  function handleGuessSelected(event) {
-    userGuess = event.detail; // Store the guess
-  }
 </script>
 
 <main>
-  <style src="./style.css"></style>
-
-  
-
-  <!-- Pass the user guess to GuessFeedback as a prop -->
-  <Autocomplete on:guessSelected={handleGuessSelected} />
-  <GuessFeedback guess={userGuess} />
+  <Autocomplete />
 </main>
